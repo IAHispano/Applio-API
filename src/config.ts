881447a1a94d202342a4782dc66677bf7044d16d
Blaise <blaise@iahispano.com>
2024-03-07 23:12:21 +0100

    add supabase to config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,11 @@ import { Hono } from "hono";
 import * as path from "path";
 
 export const app = new Hono();
+
 export const port = parseInt(process.env.PORT || "3000");
 export const routesDir = path.join(__dirname, process.env.ROUTES_DIR || "./routes");
 export const faviconPath = process.env.FAVICON_PATH || "./src/public/favicon.ico";
+
+// Supabase
+export const supabaseUrl = process.env.SUPABASE_URL || "";
+export const supabaseKey = process.env.SUPABASE_KEY || "";
