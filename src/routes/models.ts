fdcf6519599d9cd43852745ca1850059eb12f535
bygimenezz <davidalbal123@gmail.com>
2024-03-09 18:09:37 +0100

    add models

--- src/routes/models.ts
+++ src/routes/models.ts
@@ -1,21 +1,26 @@
 import { Hono } from "hono";
 import { errorHandler } from "../utils/error/errorHandler";
-import { findByName } from "../services/modelsService";
+import { findByName, getEntriesEasyPaged } from "../services/modelsService";
 
 const max_page_size = 20;
 const models = new Hono();
 
 models.get("/", async (c) => {
   try {
-    const title = c.req.header("title");
-    // const page = Number(c.req.header("page")) || 1;
-    let pageSize = Number(c.req.header("perPage"));
+    const title = c.req.header("name");
+    const page = Number(c.req.header("page")) || 1;
+    let pageSize = Number(c.req.header("perPage")) || 20;
 
     if (pageSize > max_page_size) {
       return c.text(
         `Page size cannot exceed, the max page size is ${max_page_size}.`,
         400
       );
+    }
+
+    if (!title) {
+      const data = await getEntriesEasyPaged(page, pageSize);
+      return c.json(data);
     }
 
     if (title) {
