252a4443da7db81cec7d4b04c8995ff1d67aa499
bygimenezz <davidalbal123@gmail.com>
2024-03-09 01:24:18 +0100

    add rate limit

--- src/utils/middleware/setupMiddleware.ts
+++ src/utils/middleware/setupMiddleware.ts
@@ -1,10 +1,14 @@
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
 import { prettyJSON } from "hono/pretty-json";
+import rateLimit from "../rateLimit";
 
 export const setupMiddleware = (app: any, faviconPath: string) => {
   // Middleware for logging all requests
   app.use("*", logger());
+
+  // Middleware for rate limit
+  app.use("*", rateLimit);
 
   // Middleware for pretty JSON
   app.use(prettyJSON({ space: 2 }));
---
+++ src/utils/rateLimit.ts
@@ -0,0 +1,18 @@
+import { RateLimiter } from "limiter";
+import { Context } from 'hono';
+
+const limiter = new RateLimiter({ tokensPerInterval: 150, interval: "day" });
+
+export const rateLimit = async (c: Context, next: Function) => {
+    try {
+        const remainingRequests = await limiter.removeTokens(1);
+        if (remainingRequests === 0) {
+            return c.text('Rate limit exceeded', 429);
+        }
+        await next();
+    } catch (error) {
+        return c.text('Error with rate limit', 500);
+    }
+}
+
+export default rateLimit;
