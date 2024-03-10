46e94f6906fa963e3952d74981512349b5fb792b
Blaise <blaise@iahispano.com>
2024-03-10 01:29:27 +0100

    organize

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,10 @@ export const app = new Hono();
 export const port = parseInt(process.env.PORT || "3000");
 export const routesDir = path.join(__dirname, process.env.ROUTES_DIR || "./routes");
 export const faviconPath = process.env.FAVICON_PATH || "./src/public/favicon.ico";
+
 export const maxPageSize = parseInt(process.env.MAX_PAGE_SIZE || "20");
+
+// Rates
 export const premiumRateLimit = parseInt(process.env.premiumRateLimit || "300");
 export const defaultRateLimit = parseInt(process.env.defaultRateLimit || "100");
 
